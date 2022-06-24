be1dacf67d0e3314ec69bf1500d315e946eac87d
darektoa <darektoa@gmail.com>
2022-06-25 05:03:22 +0700

    Add: Fields in blocks table migration

--- database/migrations/2022_06_27_060344_create_blocks_table.php
+++ database/migrations/2022_06_27_060344_create_blocks_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('blocks', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 20);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
