2c5d97907899bea1bb7c007465454a0bd28996ef
darektoa <darektoa@gmail.com>
2022-06-28 05:06:12 +0700

    Add: Insert dummy data in block seeder

--- database/seeders/BlockSeeder.php
+++ database/seeders/BlockSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Block;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -9,6 +10,12 @@ class BlockSeeder extends Seeder
 {
     public function run()
     {
-        //
+        $blocks = collect(['Block A', 'Block B', 'Block C', 'Block D']);
+
+        $blocks->each(fn($item) => (
+            Block::create([
+                'name'  => $item
+            ])
+        ));
     }
 }
