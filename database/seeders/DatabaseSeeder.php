4654121d6b444e47cb0bf9b6cad2e8c8de0d9de0
darektoa <darektoa@gmail.com>
2022-06-29 05:06:16 +0700

    Add: Call block and slot seeders in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,13 +7,14 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
+        $this->call([
+            BlockSeeder::class,
+            SlotSeeder::class,
+        ]);
+
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
