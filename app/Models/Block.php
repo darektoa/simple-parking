e407644fcf94318e337f5e66c55046fc73134a56
darektoa <darektoa@gmail.com>
2022-06-29 05:03:29 +0700

    Add: Slots relation method in block model

--- app/Models/Block.php
+++ app/Models/Block.php
@@ -11,4 +11,9 @@ class Block extends Model
     use HasFactory, SoftDeletes;
 
     protected $guarded = ['id'];
+
+
+    public function slots() {
+        return $this->hasMany(Slot::class);
+    }
 }
