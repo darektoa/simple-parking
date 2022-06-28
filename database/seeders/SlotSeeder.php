3730875e95d795e730cb1261ccbadb9a15b445e0
darektoa <darektoa@gmail.com>
2022-06-29 05:05:37 +0700

    Add: Insert dummy data in slot seeder

--- database/seeders/SlotSeeder.php
+++ database/seeders/SlotSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\{Block, Slot};
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -9,6 +10,14 @@ class SlotSeeder extends Seeder
 {
     public function run()
     {
-        //
+        $blocks = Block::all();
+        $slots  = [8, 10, 6, 12];
+
+        $blocks->each(function($block, $index) use($slots) {
+            $slot = $slots[$index];
+            for($i=1; $i <= $slot; $i++) $block->slots()->create([
+                'name'  => "Slot $i"
+            ]);
+        });
     }
 }
