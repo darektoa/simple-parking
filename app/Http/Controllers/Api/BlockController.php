b553fa8e0945cbe20525a0c125b98a5f6e41bdcf
darektoa <darektoa@gmail.com>
2022-07-01 05:03:18 +0700

    Add: Index method in block api controller

--- app/Http/Controllers/Api/BlockController.php
+++ app/Http/Controllers/Api/BlockController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BlockResource;
+use App\Models\Block;
 use Illuminate\Http\Request;
 
 class BlockController extends Controller
 {
-    //
+    public function index() {
+        $blocks = Block::get();
+
+        return ResponseHelper::make(
+            BlockResource::collection($blocks)
+        );
+    }
 }
