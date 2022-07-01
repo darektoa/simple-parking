e70a049812216150ab803f2360eef04d508dc144
darektoa <darektoa@gmail.com>
2022-07-02 05:04:16 +0700

    Add: Index method in slot API controller

--- app/Http/Controllers/Api/SlotController.php
+++ app/Http/Controllers/Api/SlotController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SlotResource;
+use App\Models\Slot;
 use Illuminate\Http\Request;
 
 class SlotController extends Controller
 {
-    //
+    public function index() {
+        $slots = Slot::with('block')->get();
+
+        return ResponseHelper::make(
+            SlotResource::collection($slots)
+        );
+    }
 }
