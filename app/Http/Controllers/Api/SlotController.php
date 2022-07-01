66cdccf6eddafeeb99abe079306d3c6d0f0e080d
darektoa <darektoa@gmail.com>
2022-07-02 05:06:48 +0700

    Add: Show method in slot API controller

--- app/Http/Controllers/Api/SlotController.php
+++ app/Http/Controllers/Api/SlotController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Exceptions\ErrorException;
 use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SlotResource;
@@ -16,5 +17,22 @@ class SlotController extends Controller
         return ResponseHelper::make(
             SlotResource::collection($slots)
         );
+    }
+
+
+    public function show(Slot $slot) {
+        try{
+            if(!$slot) throw new ErrorException('Not Found', 404);
+
+            return ResponseHelper::make(
+                SlotResource::make($slot->load('block'))
+            );
+        }catch(ErrorException $err) {
+            return ResponseHelper::error(
+                $err->getErrors(),
+                $err->getMessage(),
+                $err->getCode(),
+            );
+        }
     }
 }
