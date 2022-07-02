d2ce64817b2756f9da75f0c0a74760ca20445263
darektoa <darektoa@gmail.com>
2022-07-03 05:06:15 +0700

    Add: Enter method in parking API controller

--- app/Http/Controllers/Api/ParkingController.php
+++ app/Http/Controllers/Api/ParkingController.php
@@ -2,10 +2,42 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Exceptions\ErrorException;
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SlotResource;
+use App\Models\Slot;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ParkingController extends Controller
 {
-    //
+    public function enter(Request $request) {
+        try{
+            $validator = Validator::make($request->all(), [
+                'slot_id'   => 'exists:slots,id',
+            ]);
+
+            if($validator->fails()) {
+                $errors = $validator->errors()->all();
+                throw new ErrorException('Unproccessable', 422, $errors);
+            }
+
+            $slot = Slot::find((int) $request->slot_id);
+            $slot->update([
+                'status'    => 2,
+                'used_at'   => now(),
+            ]);
+
+            return ResponseHelper::make(
+                SlotResource::make($slot)
+            );
+        }catch(ErrorException $err) {
+            return ResponseHelper::error(
+                $err->getErrors(),
+                $err->getMessage(),
+                $err->getCode(),
+            );
+        }
+    }
 }
